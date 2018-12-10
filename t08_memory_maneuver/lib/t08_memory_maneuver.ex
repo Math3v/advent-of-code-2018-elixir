ac3db059a6df74a5a9aedae27eaa1888d3fc77ab
Matej Minarik <mato.minarik@azet.sk>
2018-12-10 21:44:13 +0100

    08 Memory Maneuver with Sasha Juric help

--- t08_memory_maneuver/lib/t08_memory_maneuver.ex
+++ t08_memory_maneuver/lib/t08_memory_maneuver.ex
@@ -1,53 +1,35 @@
 defmodule T08MemoryManeuver do
-  def parse_node(input) do
-    [nchildren | [nmeta | meta]] =
-      input
-      |> String.split(" ")
-      |> Enum.map(&String.to_integer/1)
-
-    {nchildren, nmeta, meta}
+  def call do
+    "./input.txt"
+    |> File.read!()
+    |> String.split()
+    |> Enum.map(&String.to_integer/1)
+    |> sum_nodes()
   end
 
-  def split_children(children) do
-    split_children(children, [])
+  def sum_nodes(nodes) do
+    {sum, _} = next_node(nodes)
+    sum
   end
 
-  def split_children([], result), do: result
-
-  def split_children(children, result) do
-    size = node_size(children)
-    {child, rest} = Enum.split(children, size + 2)
-    split_children(rest, result ++ [child])
+  defp next_node(numbers) do
+    [num_children, num_metas | numbers] = numbers
+    {children, numbers} = next_nodes(numbers, num_children)
+    {metas, numbers} = Enum.split(numbers, num_metas)
+    sum = Enum.sum(metas) + Enum.sum(children)
+    {sum, numbers}
   end
 
-  def sum_nodes([]), do: {0, []}
+  defp next_nodes(numbers, nchildren) do
+    {reversed_children, numbers} =
+      [nil]
+      |> Stream.cycle()
+      |> Stream.take(nchildren)
+      |> Enum.reduce({[], numbers}, fn _, {children, numbers} ->
+        {child, numbers} = next_node(numbers)
+        {[child | children], numbers}
+      end)
 
-  def sum_nodes([0 | [nmeta | meta]]) do
-    {to_sum, rest} =
-      meta
-      |> Enum.reverse()
-      |> Enum.split(nmeta)
-
-    {Enum.sum(to_sum), rest}
-  end
-
-  def sum_nodes([nchildren | [nmeta | meta]] = nodes) do
-    {to_sum, rest} =
-      meta
-      |> Enum.reverse()
-      |> Enum.split(nmeta)
-
-    {Enum.sum(to_sum) + sum_nodes(rest), rest}
-  end
-
-  defp node_size([_ | [size | _]]), do: size
-
-  defp sum_last_n_and_return_rest(list, n) do
-    {to_sum, rest} =
-      list
-      |> Enum.reverse()
-      |> Enum.split(n)
-
-    {Enum.sum(to_sum), Enum.reverse(rest)}
+    {Enum.reverse(reversed_children), numbers}
   end
 end
--- t08_memory_maneuver/test/t08_memory_maneuver_test.exs
+++ t08_memory_maneuver/test/t08_memory_maneuver_test.exs
@@ -1,25 +1,6 @@
 defmodule T08MemoryManeuverTest do
   use ExUnit.Case
 
-  test "parses tree node" do
-    assert T08MemoryManeuver.parse_node("0 3 10 11 12") == {0, 3, [10, 11, 12]}
-  end
-
-  test "splits children" do
-    assert T08MemoryManeuver.split_children([0, 1, 99, 0, 1, 99]) == [[0, 1, 99], [0, 1, 99]]
-
-    assert T08MemoryManeuver.split_children([0, 1, 99, 0, 2, 9, 9]) == [
-             [0, 1, 99],
-             [0, 2, 9, 9]
-           ]
-
-    assert T08MemoryManeuver.split_children([0, 1, 99, 0, 2, 9, 9, 0, 1, 99]) == [
-             [0, 1, 99],
-             [0, 2, 9, 9],
-             [0, 1, 99]
-           ]
-  end
-
   test "sums meta for childless node" do
     assert T08MemoryManeuver.sum_nodes([0, 3, 10, 11, 12]) == 33
   end
@@ -29,7 +10,7 @@ defmodule T08MemoryManeuverTest do
   end
 
   test "sums meta for two-child node" do
-    assert T08MemoryManeuver.sum_nodes([2, 1, 0, 1, 99, 0, 1, 99, 2]) == 101
+    assert T08MemoryManeuver.sum_nodes([2, 1, 0, 1, 99, 0, 1, 99, 2]) == 200
   end
 
   test "sums example" do
